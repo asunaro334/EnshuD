e3cff56a3dae68f85d38cf8eb56ef88cf84dc791
DESKTOP-VF8N1O3\shinya <shinyacrash1995@gmail.com>
2017-01-12 13:45:42 +0900

    自宅作業分

--- Parser/src/parser/Items/ProgramItem.java
+++ Parser/src/parser/Items/ProgramItem.java
@@ -7,37 +7,52 @@ import parser.Types.Pair4;
 
 public class ProgramItem {
 	public ProgramItem(ArrayList<Pair4<String, String, Short, Short>> tokens) throws SyntaxException {
-		int i;
+		int i = 0,j,nest=0;
 		NameRowItem names;
+		StatementItem state;
+		BlockItem block;
+
+		if(!"SDOT".equals(tokens.get(tokens.size() - 1).getML()))
+			throw new SyntaxException(tokens.get(tokens.size() - 1).getRR());
 
 		if(!"SPROGRAM".equals(tokens.get(0).getML()))
 			throw new SyntaxException(tokens.get(0).getRR());
 
 		if(!"SIDENTIFIER".equals(tokens.get(1).getML()))
 			throw new SyntaxException(tokens.get(1).getRR());
-		else if(!"SLPAREN".equals(tokens.get(1).getML()))
+		else if(!"SLPAREN".equals(tokens.get(2).getML()))
 			throw new SyntaxException(tokens.get(2).getRR());
 
 		for(i = 3; i < tokens.size(); i++){
-			if(i == tokens.size() - 1){
-				throw new SyntaxException(tokens.get(tokens.size() - 1).getRR());
-			}
 			if("SRPAREN".equals(tokens.get(i).getML())){
 				names = new NameRowItem((ArrayList<Pair4<String, String, Short, Short>>)tokens.subList(3, i));
-				i++;
 				break;
 			}
 		}
 
-		if(i == tokens.size() - 1)
-			throw new SyntaxException(tokens.get(i).getRR());
+		i++;
 
 		if(!"SSEMICOLON".equals(tokens.get(i).getML()))
 			throw new SyntaxException(tokens.get(i).getRR());
-		
-		
 
-		if(!"SDOT".equals(tokens.get(tokens.size() - 1).getML()))
-			throw new SyntaxException(tokens.get(tokens.size() - 1).getRR());
+		j = tokens.size() - 2;
+
+		for(j = tokens.size() - 2; j >= i; j--){
+			if(j == i)
+				throw new SyntaxException(tokens.get(j).getRR());
+			if("SEND".equals(tokens.get(j).getML()))
+				nest++;
+			else if("SBEGIN".equals(tokens.get(j).getML()))
+				nest--;
+
+			if(nest == 0){
+				state = new StatementItem((ArrayList<Pair4<String, String, Short, Short>>)tokens.subList(j, tokens.size() - 1));
+				break;
+			}
+		}
+
+		if(j > i + 1){
+			block = new BlockItem((ArrayList<Pair4<String, String, Short, Short>>)tokens.subList(i + 1, j));
+		}
 	}
 }
---
+++ Parser/src/parser/Items/StatementItem.java
@@ -0,0 +1,11 @@
+package parser.Items;
+
+import java.util.ArrayList;
+
+import parser.Types.Pair4;
+
+public class StatementItem {
+	public StatementItem(ArrayList<Pair4<String, String, Short, Short>> tokens) {
+
+	}
+}
